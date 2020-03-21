428b76fae213e3f59d9b3c5b7587bc2a247776a2
jyothi-kasturi <jyothifeb1@gmail.com>
2020-03-21 00:10:15 +0000

    Fixed a subnet error

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -2,7 +2,8 @@ module "assignment_vpc" {
   source = "../modules/vpc"
   
   vpc_cidr = "198.120.0.0/16"
-  subnet_cidr = "198.120.1.0/24"
+  subnet_public_cidr = "198.120.1.0/24"
+  subnet_private_cidr = "198.120.1.0/24"
   tenancy = "default"
   vpc_id = "${module.assignment_vpc.vpc_id}"
   project_name = "assignment"
