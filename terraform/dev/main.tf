7b16606918bb8acbaa4da20ae6907ced898f7633
jyothi-kasturi <jyothifeb1@gmail.com>
2020-03-21 00:22:24 +0000

    Fixed The CIDR '198.120.1.0/24' conflicts with another subnet

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,7 +3,7 @@ module "assignment_vpc" {
   
   vpc_cidr = "198.120.0.0/16"
   subnet_public_cidr = "198.120.1.0/24"
-  subnet_private_cidr = "198.120.1.0/24"
+  subnet_private_cidr = "198.120.2.0/24"
   tenancy = "default"
   vpc_id = "${module.assignment_vpc.vpc_id}"
   project_name = "assignment"
